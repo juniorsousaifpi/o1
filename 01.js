0f8b49e90d745685e828997c787cee8c0c67ca64
ian gabriel <ianzoka763@gmail.com>
2024-12-02 19:23:16 -0300

    first commit

---
+++ 01.js
@@ -0,0 +1,13 @@
+//solicite ao usuario que solicite um numero
+let  numero = prompt('digite um númeo:');
+
+//converte a entrada para um numero inteiro
+numero = parseInt;
+//verifica se numero é par ou impar
+if (numero % 2 === 0){
+    console.log("o numero é par.");
+} else{
+    console.log("onumero é impar.");
+    
+}
+
---
+++ 02.js
@@ -0,0 +1,9 @@
+//solicite ao usuario que insira trés numeros
+
+let num1 = parseFloat(prompt("digite o primeiro numero:"));
+let num2 = parseFloat(prompt("digite o segundo numero;"));
+let num3 = parseFloat(prompt("digite o terceiro numero"));
+ //determine o mmaior numero usando a função math.max
+ let maior = Math.max(num1, num2, num3,);
+ //implimae o maior numero 
+ console.log("o maior numero é:" + maior);
